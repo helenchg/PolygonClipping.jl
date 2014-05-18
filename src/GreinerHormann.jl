077f73dd5697e20c357c8b989b99955054b103af
Steve Kelly <kd2cca@gmail.com>
2014-05-18 18:53:59 -0400

    some work on data structure and tests for the doubly linked list

--- src/GreinerHormann.jl
+++ src/GreinerHormann.jl
@@ -1,5 +1,48 @@
 module GreinerHormann
+import Base.show
 
-# package code goes here
+type Vertex
+    location::Array
+    next
+    prev
+    nextpoly
+    intersect::Bool
+    entry::Bool
+    neighbor
+    alpha::Float64
 
+    Vertex(x) = new(x, None, None, None, false, false, None, 0.0)
+end
+
+type Polygon
+    start
+
+    Polygon() = new(None)
+end
+
+function show(io::IO, p::Polygon)
+    vert = p.start
+    print("Polygon(")
+    while vert != None
+        print(vert.location)
+        vert = vert.next
+    end
+    print(")")
+end
+
+function push!(p::Polygon, v::Vertex)
+    if p.start == None
+        p.start = v
+    else
+        tail = p.start
+        while tail.next != None
+            tail = tail.next
+        end
+        v.prev = tail
+        tail.next = v
+    end
+end
+
+
+export Vertex, Polygon, push!
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using GreinerHormann
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Test Doubly linked list
+println("Testing doubly linked list")
+poly = Polygon()
+for i = 1:10
+    push!(poly, Vertex(rand(2)))
+end
+vert1 = poly.start
+vert2 = vert1.next
+for i = 1:9
+    @test is(vert1, vert2.prev)
+    vert1 = vert1.next
+    vert2 = vert2.next
+end
+
