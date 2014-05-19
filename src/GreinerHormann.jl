5d62f57064e70bd0029834340d25a224568f77af
Steve Kelly <kd2cca@gmail.com>
2014-05-19 12:19:09 -0400

    dirty commit before trying different point in polygon algorithm

--- src/GreinerHormann.jl
+++ src/GreinerHormann.jl
@@ -1,8 +1,9 @@
 module GreinerHormann
 import Base.show
+import Base.push!
 
 type Vertex
-    location::Array
+    location::Array{Float64}
     next
     prev
     nextpoly
@@ -11,7 +12,7 @@ type Vertex
     neighbor
     alpha::Float64
 
-    Vertex(x) = new(x, None, None, None, false, false, None, 0.0)
+    Vertex(x) = new(x, None, None, None, false, true, None, 0.0)
 end
 
 ################################################################################
@@ -36,12 +37,21 @@ end
 
 function show(io::IO, p::Polygon)
     vert = p.start
-    print("Polygon(")
+    print("Polygon:")
     while vert != None
-        print(vert.location)
+        println("Vertex:")
+        println("\tMem:", pointer_from_objref(vert))
+        println("\tLocation:",vert.location)
+        println("\tNext:",pointer_from_objref(vert.next))
+        println("\tPrev:",pointer_from_objref(vert.prev))
+        println("\tNextPoly:",pointer_from_objref(vert.nextpoly))
+        println("\tNeighbor:",pointer_from_objref(vert.neighbor))
+        println("\tIntersect:",vert.intersect)
+        println("\tEntry:",vert.entry)
+        println("\tAlpha:",vert.alpha)
         vert = vert.next
     end
-    print(")")
+    print("")
 end
 
 function push!(p::Polygon, v::Vertex)
@@ -70,5 +80,112 @@ function Vertex(s::Vertex, c::Vertex, alpha)
     return a
 end
 
-export Vertex, Polygon, push!
+function isinside(v::Vertex, p::Polygon)
+    # See: http://www.ecse.rpi.edu/~wrf/Research/Short_Notes/pnpoly.html
+    test = v.location
+    vert = p.start
+    locs = Array{Float64}[]
+    nvert = 0
+    while vert.next != None
+        println(vert.location)
+        push!(locs, vert.location)
+        nvert = nvert + 1
+        vert = vert.next
+    end
+    # Close polygon
+    push!(locs, vert.location)
+    i = 1
+    j = nvert
+    c = false
+   # println(locs)
+    while i < nvert
+   #     println("i:",i)
+   #     println("j:",j)
+        if ( ((locs[i][2]>test[2]) != (locs[j][2]>test[2])) &&
+            (test[1] < (locs[j][1]-locs[i][1]) * (test[2]-locs[i][2]) / (locs[j][2]-locs[i][2]) + locs[i][1]) )
+            c = !c
+        end
+        j = i
+        i = i + 1
+    end
+    return c
+end
+
+function clip(subject::Polygon, clip::Polygon)
+    # Phase 1
+    sv = subject.start
+    cv = clip.start
+    while sv.next != None
+        while cv.next != None
+            println("finding intersection")
+            intersect, a, b = intersection(sv, sv.next, cv, cv.next)
+            println(intersect, a, b)
+            if intersect
+                i1 = Vertex(cv, cv.next, a)
+                i2 = Vertex(sv, sv.next, b)
+                i1.intersect = true
+                i2.intersect = true
+                i1.neighbor = i2
+                i2.neighbor = i1
+                cv = i1.next # make sure we hop over the vertex we just inserted
+            else
+                cv = cv.next
+            end
+        end
+        sv = sv.next
+    end
+
+    # Phase 2
+    status = false
+    if isinside(subject.start, clip)
+        status = false
+    else
+        status = true
+    end
+    sv = subject.start
+    while sv.next != None
+        if sv.intersect
+            sv.entry = status
+            status = !status
+        end
+        sv = sv.next
+    end
+
+    status = false
+    if isinside(clip.start, subject)
+        status = false
+    else
+        status = true
+    end
+    cv = clip.start
+    while cv.next != None
+        if cv.intersect
+            cv.entry = status
+            status = !status
+        end
+        cv = cv.next
+    end
+
+end
+
+
+function intersection(sv, svn, cv, cvn)
+    s1 = sv.location
+    s2 = svn.location
+    c1 = cv.location
+    c2 = cvn.location
+
+    den = (c2[2] - c1[2]) * (s2[1] - s1[1]) - (c2[1] - c1[1]) * (s2[2] - s1[2])
+    if den == 0
+        return false, 0, 0
+    end
+    a = ((c2[1] - c1[1]) * (s1[2] - c1[2]) - (c2[2] - c1[2]) * (s1[1] - c1[1])) / den
+    b = ((s2[1] - s1[1]) * (s1[2] - c1[2]) - (s2[2] - s1[2]) * (s1[1] - c1[1])) / den
+
+    return true, a, b
+
+end
+
+
+export Vertex, Polygon, push!, clip, intersection, isinside
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,8 @@ for i = 1:10
     vert1 = vert1.next
     vert2 = vert2.next
 end
+
+
 # test we close the polygon and terminate with None
 @test poly.start.location == poly.finish.next.location
 @test poly.finish.next.next == None
@@ -34,3 +36,45 @@ a = Vertex(poly.start, poly.start.next, 0.25)
 @test vert1.next === a
 @test vert2.prev === a
 
+# test isinside method
+println("Testing point in polygon...")
+poly1 = Polygon()
+push!(poly1, Vertex([0,0]))
+push!(poly1, Vertex([1,0]))
+push!(poly1, Vertex([1,1]))
+push!(poly1, Vertex([0,1]))
+@test isinside(Vertex([0.5,0.5]), poly1) == true
+@test isinside(Vertex([-0.5,0.5]), poly1) == false
+
+# add self intersections
+push!(poly1, Vertex([0.5,0.5]))
+@test isinside(Vertex([0.4,0.5]), poly1) == false
+@test isinside(Vertex([0.75,0.75]), poly1) == true
+@test isinside(Vertex([0.75,0.25]), poly1) == true
+
+
+poly2 = Polygon()
+push!(poly2, Vertex([0.5,0.25]))
+push!(poly2, Vertex([1.5,0.25]))
+push!(poly2, Vertex([1.5,-0.75]))
+push!(poly2, Vertex([0.5,-0.75]))
+println(poly2)
+result = clip(poly1, poly2)
+#println(result[1])
+
+
+# test intersection method
+@test (intersection(Vertex([0,0]), Vertex([1,1]), Vertex([1,0]), Vertex([0,1]))
+        == (true, 0.5, 0.5))
+@test (intersection(Vertex([0,0]), Vertex([1,1]), Vertex([1/2,0]), Vertex([0,1/2]))
+        == (true, 0.25, 0.5))
+@test (intersection(Vertex([0,0]), Vertex([2,2]), Vertex([1/2,0]), Vertex([0,1/2]))
+        == (true, 0.125, 0.5))
+@test (intersection(Vertex([0,0]), Vertex([0,1]), Vertex([1,0]), Vertex([1,1]))
+        == (false, 0, 0))
+@test (intersection(Vertex([0.5,0]), Vertex([0.5,1]), Vertex([0,0.5]), Vertex([1,0.5]))
+        == (true, 0.5, 0.5))
+@test (intersection(Vertex([0,0]), Vertex([1,1]), Vertex([0,0]), Vertex([-1,-1]))
+        == (false, 0, 0))
+@test (intersection(Vertex([0,0]), Vertex([0,0]), Vertex([1,1]), Vertex([1,1]))
+        == (false, 0, 0))
